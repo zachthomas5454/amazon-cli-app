3d6a2d30f74dc8e3093a17ea24a57a8fde50f47f
Zach <zachariahthomas54@yahoo.com>
2020-01-11 01:06:33 -0500

    Commit after testing

--- amazon.sql
+++ amazon.sql
@@ -6,43 +6,43 @@ USE bamazon;
 
 CREATE TABLE products (
   
-  item_id VARCHAR(15) NOT NULL,
+  item_id INTEGER(10) NOT NULL,
   product_name VARCHAR(30),
   department_name VARCHAR(30),
-  price INTEGER(10),
-  stock_quantity INTEGER(10)
+  price DECIMAL(10,2),
+  stock_quantity INTEGER(10) NOT NULL
 
 );
 
 
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("1A", "Amazon Echo", "Electronics", 100, 1 );
+VALUES (11, "Amazon Echo", "Electronics", 100, 289 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("1B", "US Polo T-Shirt", "Clothing", 38, 2 );
+VALUES (13, "US Polo T-Shirt", "Clothing", 38, 8 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("3C", "Wireless Mouse", "Electronics", 25, 1 );
+VALUES (31, "Wireless Mouse", "Electronics", 25, 77 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("1H", "Fossils Watch", "Dress Accessories", 120, 1 );
+VALUES (17, "Fossils Watch", "Dress Accessories", 120, 104 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("6W", "Hohner Harmonica", "Musical Instrument", 65, 1 );
+VALUES (68, "Hohner Harmonica", "Musical Instrument", 65, 62 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("8Y", "Hallmark Birthday Card", "Gifts", 8, 2 );
+VALUES (89, "Hallmark Birthday Card", "Gifts", 8, 335 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("7O", "350 pc Lego", "Toys", 235, 1 );
+VALUES (70, "350 pc Lego", "Toys", 235, 23 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("9M", "Lunch Kit", "Kitchen", 25, 1 );
+VALUES (96, "Lunch Kit", "Kitchen", 25, 33 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("4E", "Criminal Minds", "Prime Video", 14, 1 );
+VALUES (43, "Criminal Minds", "Prime Video", 14, 7 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("2U", "Beginner's guide to Violin", "Books", 18, 1 );
+VALUES (28, "Beginner's guide to Violin", "Books", 18, 8 );
 
